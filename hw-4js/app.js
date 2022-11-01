e17f11ef0d60869aa97e47cf33e775aa37d3e32d
denisgit122 <yaholnykden@gmail.com>
2022-11-01 10:34:19 +0200

    add menu

---
+++ hw-4js/app.js
@@ -0,0 +1,51 @@
+let hello='hello'
+console.log(hello);
+let owu='owu'
+console.log(owu);
+let com='com'
+console.log(com);
+let ua='ua'
+console.log(ua);
+let one = 1
+console.log(one);
+let ten=10
+console.log(ten);
+let min=-999
+console.log(min);
+let num=123
+console.log(num);
+const PI=3.14
+console.log(PI);
+const e=2.7
+console.log(e);
+const sixteen=16
+console.log(sixteen);
+
+
+let name='Denis';
+let surname='Yuriyovych';
+let lastName='Yaholnyk';
+let data = name +' '+ surname +' '+ lastName;
+console.log(data)
+
+
+
+let a=100;
+console.log(typeof a)
+let b='100';
+console.log(typeof b)
+let c='істина';
+console.log(typeof c)
+
+
+var person
+if(confirm('hello')){
+    person=prompt('Hi what`s your name?')
+    surname2=prompt(' what`s your surname?')
+    age2=prompt(' how old are you?')
+    alert('hello,' + person  +' '+ surname2  +' '+ age2)
+}
+
+// else
+    //alert('person')
+
